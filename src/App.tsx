18dc2af7da9c58282dadb7eba815b9d6893d102c
Ticiana Capris <tcapris@hotmail.com>
2022-07-06 18:17:36 +0100

    fim desafio

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
+import React, { useState } from "react";
+
 const App = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const[sobrenome, setSobrenome] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
+  const handleChangeAge = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(Number(event.target.value));
+  }
+  const handleChangeSobrenome = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSobrenome(event.target.value);
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text"value={name} onChange={handleChange}/>
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={sobrenome} onChange={handleChangeSobrenome} />
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value ={age} onChange={handleChangeAge}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {name}, tudo bem?<br/>
+      Você tem {age} anos.
     </div>
   );
 }
